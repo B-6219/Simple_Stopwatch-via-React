f4fce64e1bb8c37d0984631840b1eff782aa7236
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-05-23 10:25:11 +0100

    second commit

--- react_stopwatch/src/Stopwatch.jsx
+++ react_stopwatch/src/Stopwatch.jsx
@@ -5,26 +5,54 @@ function Stopwatch(){
 
     const [isRunning , setIsRunning] = useState(false)
     const [elapsedTime, setElapsedTime] = useState(0)
-    const  internalIdRef = useRef(null)
+    const intervalIdRef = useRef(null)
     const startTimeRef = useRef(0)
 
     useEffect( () => {
 
+        if (isRunning) {
+
+            startTimeRef.current = Date.now() - elapsedTime;
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 100);
+        }
+        else {
+            clearInterval(intervalIdRef.current);
+        }
+
+    return () => clearInterval(intervalIdRef.current);
+
     },[isRunning])
 
     function start(){
-
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime
     }
+
+
     function  reset(){
-
+        setElapsedTime(0)
+        setIsRunning(false)
     }
-    function stop(){
 
+
+    function stop(){
+        setIsRunning(false)
     }
 
     function formatTime(){
-        return "00.00.00"
-    }
+
+        const totalMilliseconds = elapsedTime;
+        const seconds = Math.floor((totalMilliseconds / 1000) % 60);
+        const minutes = Math.floor((totalMilliseconds / 60000) % 60);
+        const hours = Math.floor(totalMilliseconds / 3600000);
+
+        return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+        2,
+        "0"
+        )}:${String(seconds).padStart(2, "0")}`;
+        }
     
     return(
         <div className="stopwatch">
@@ -34,9 +62,9 @@ function Stopwatch(){
             <div className="controls">
                 <button onClick={start}  className="start_btn">Start</button>
                  <button onClick={reset}  className="reset_btn">Reset</button>
-                  <button onClick={stop}  className="start_btn">Stop</button>
+                  <button onClick={stop}  className="stop_btn">Stop</button>
             </div>
-            
+
         </div>
     )
 }
